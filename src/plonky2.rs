a12f00d68547d006491521c3b66d6d770c1e2783
nikkolasg <nikkolasg@gmail.com>
2023-09-16 18:03:07 -0400

    working multi level recursion

--- src/plonky2.rs
+++ src/plonky2.rs
@@ -103,8 +103,57 @@ where
 
     Ok((proof, data.verifier_only, data.common))
 }
+
+fn recursive_sha<
+    F: RichField + Extendable<D>,
+    C: GenericConfig<D, F = F>,
+    InnerC: GenericConfig<D, F = F>,
+    const D: usize,
+>(
+    inner: &ProofTuple<F, InnerC, D>,
+    config: &CircuitConfig,
+    input: Vec<u8>,
+) -> Result<ProofTuple<F, C, D>>
+where
+    // Need to ensure we can efficiently compute the hash inside the circuit
+    InnerC::Hasher: AlgebraicHasher<F>,
+{
+    let (inner_proof, inner_vd, inner_cd) = inner;
+    let mut builder = CircuitBuilder::<F, D>::new(config.clone());
+    // 1. verify previous proof
+    let pt = builder.add_virtual_proof_with_pis(inner_cd);
+    let inner_data = builder.add_virtual_verifier_data(inner_cd.config.fri_config.cap_height);
+    builder.verify_proof::<InnerC>(&pt, &inner_data, inner_cd);
+    builder.print_gate_counts(0);
+    // 2. compute sha256
+    let target_input = builder.add_virtual_hash_input_target(2, 512);
+    let target_output = builder.sha256(&target_input);
+
+    let data = builder.build::<C>();
+
+    let mut pw = PartialWitness::new();
+    // connect proof verification inputs
+    pw.set_proof_with_pis_target(&pt, inner_proof);
+    pw.set_verifier_data_target(&inner_data, inner_vd);
+
+    // connect sha256 inputs
+    let output = Sha256::new().chain(input.clone()).finalize();
+    pw.set_sha256_input_target(&target_input, &input);
+    pw.set_sha256_output_target(&target_output, &output);
+
+    let mut timing = Instant::now();
+    let mut tree = TimingTree::new("prove", Level::Debug);
+    let proof = prove::<F, C, D>(&data.prover_only, &data.common, pw, &mut tree)?;
+    let time_recursive_prove = timing.elapsed();
+
+    debug_assert!(data.verify(proof.clone()).is_ok());
+
+    Ok((proof, data.verifier_only, data.common))
+}
+
 #[cfg(test)]
 mod tests {
+    use anyhow::Ok;
     use plonky2::iop::witness::PartialWitness;
     use plonky2::plonk::circuit_builder::CircuitBuilder;
     use plonky2::plonk::circuit_data::CircuitConfig;
@@ -114,16 +163,30 @@ mod tests {
     use std::time::{Duration, Instant};
 
     use super::*;
+    const D: usize = 2;
+    type C = PoseidonGoldilocksConfig;
+    type F = <C as GenericConfig<D>>::F;
+
+    #[test]
+    fn test_sha_recursion() -> Result<()> {
+        // we generate a first proof to verify
+        // TODO: find the "empty proof" equivalence ?
+        let input1 = hex::decode("600D54000000000000000000000000000000000000000000000000000000000077F1040000000000000000000000000000000000000000000000000000000000").unwrap();
+        let config = CircuitConfig::standard_recursion_config();
+        let inner = generate_sha256_proof::<F, C, D>(&config, input1.clone())?;
+        // now we can start recursively create proof that (a) verify previous proof and (b) compute sha256 iteration
+        let lvl1 = recursive_sha::<F, C, C, D>(&inner, &config, input1.clone())?;
+        let lvl2 = recursive_sha::<F, C, C, D>(&lvl1, &config, input1.clone())?;
+        let lvl3 = recursive_sha::<F, C, C, D>(&lvl2, &config, input1.clone())?;
+        Ok(())
+    }
 
     #[test]
     fn test_simple_recursion() -> Result<()> {
-        const D: usize = 2;
-        type C = PoseidonGoldilocksConfig;
-        type F = <C as GenericConfig<D>>::F;
         let input = hex::decode("600D54000000000000000000000000000000000000000000000000000000000077F1040000000000000000000000000000000000000000000000000000000000").unwrap();
         let config = CircuitConfig::standard_recursion_config();
         let inner = generate_sha256_proof::<F, C, D>(&config, input)?;
-        let middle = recursive_proof::<F, C, C, D>(&inner, &config)?;
+        let outer = recursive_proof::<F, C, C, D>(&inner, &config)?;
         Ok(())
     }
 
